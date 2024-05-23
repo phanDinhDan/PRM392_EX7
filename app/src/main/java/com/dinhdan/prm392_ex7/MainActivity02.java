5e24e6cde7a6e4d3bc08068c8673475ac4a60127
phanDinhDan <phandinhdan6602@gmail.com>
2024-05-23 13:51:54 +0700

    first commit

---
+++ app/src/main/java/com/dinhdan/prm392_ex7/MainActivity02.java
@@ -0,0 +1,33 @@
+package com.dinhdan.prm392_ex7;
+
+import android.os.Bundle;
+import android.widget.TextView;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity02 extends AppCompatActivity {
+    public TextView tvShow;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main02);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main
+        ), (v, insets) -> {
+            Insets systemBars =
+                    insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top,
+                    systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        tvShow = (TextView) findViewById(R.id.tvShow);
+        if (getIntent()!=null) {
+            String data = getIntent().getStringExtra("MESSAGE");
+            tvShow.setText("Hello " + data);
+        }
+    }
+}
